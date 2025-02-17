c7da2cc8ae58828ffa887c26abbe67b50dd5044f
Bina <benjaminbinyam500@gmail.com>
2025-02-17 15:17:10 +0300

    Add filtering items feature

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,19 @@ const App = () => {
   const [menuItem, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
   // console.log(categories);
-
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const filteredItems = items.filter((item) => item.category === category);
+    setMenuItems(filteredItems);
+  };
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItem} />
       </section>
     </main>
--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,9 +1,14 @@
-const Categories = ({ categories }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
       {categories.map((category) => {
         return (
-          <button type="button" key={category} className="btn">
+          <button
+            type="button"
+            key={category}
+            className="btn"
+            onClick={() => filterItems(category)}
+          >
             {category}
           </button>
         );

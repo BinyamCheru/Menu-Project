bb6072e6b909b76905304e8a07360306fe5e58b4
Bina <benjaminbinyam500@gmail.com>
2025-02-17 11:47:27 +0300

    Create unique categories using Set

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import Title from "./Title";
 import items from "./data";
 import Menu from "./Menu";
 
+// const tempCategories = items.map((item) => item.category);
+// console.log(tempCategories);
+// const tempSet = new Set(tempCategories);
+// console.log(tempSet);
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+// shorter way to filter unique categories
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
 const App = () => {
   const [menuItem, setMenuItems] = useState(items);
   return (
--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,12 +1,14 @@
 const MenuItem = ({ img, title, price, desc }) => {
   return (
-    <article className="item-info">
+    <article className="menu-item">
       <img src={img} alt={title} className="img" />
-      <header>
-        <h5> {title} </h5>
-        <span className="item-price">${price}</span>
-        <p className="item-text">{desc} </p>
-      </header>
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <span className="item-price">${price}</span>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
     </article>
   );
 };
